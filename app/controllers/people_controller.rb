1672dd236457f22f12e9f63de0b6e70899eda1ff
Houndsto0th <bry.kinnear@gmail.com>
2014-10-24 16:31:32 -0600

    fixed base load

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,14 +12,20 @@ class PeopleController < ApplicationController
           eye_color: %w(brown blue hazel green).sample,
           telephone_number: Faker::PhoneNumber.phone_number
         }
-      end.sort_by! do |x|
-        x[params[:sort_by].to_sym]
+      end.sort_by! do |people|
+        people[params[:sort_by].to_sym]
       end
 
     else
-
-      @people
-
+      @people = 30.times.map do
+      person = {
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        date_of_birth: (20.years.ago.to_date..4.years.ago.to_date).to_a.sample,
+        eye_color: %w(brown blue hazel green).sample,
+        telephone_number: Faker::PhoneNumber.phone_number
+      }
+    end
     end
 
   end
